1f60793244a326f6bb8ffa43d1ad6cbe64c7916d
Matteo <Rascioni>
2021-02-10 19:09:24 +0100

    API call background Hero

--- netflix-app/src/UI/Banner/Banner.js
+++ netflix-app/src/UI/Banner/Banner.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../../axios';
+import requests from '../../Requests';
 
 import './Banner.css'
 
 const Banner = () => {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+                const request = await axios.get(requests.fetchNetflixOriginals);
+                setMovie(request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(movie)
+
+    // MAX 150 STRING FUNCTION
     const truncate = (string, n) => {
         return string?.length > n ? string.substr(0, n - 1) + '...' : string;
     }
-    
+
     return (
         <header 
             className="banner" 
             style={{
                 backgroundSize: "cover",
-                backgroundImage: `url("https://i.imgur.com/e1hLQ2m.png")`,
-                backgroundPosition: "center, center"
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center, center",
             }}
         >
             <div className="banner__content">
@@ -31,7 +51,7 @@ const Banner = () => {
                 </h1>
             </div>
 
-            <div className="banner--fadeBottom" />
+            <div className="banner__fadeBottom" />
         </header>
     )
 }
