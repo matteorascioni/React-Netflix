e53f9148ba8326e8b43b3dae130682cd400c23d6
Matteo <Rascioni>
2021-02-11 20:28:56 +0100

    LogIn form 50%

--- netflix-app/src/UI/Screens/SignUpScreen/SignUpScreen.js
+++ netflix-app/src/UI/Screens/SignUpScreen/SignUpScreen.js
@@ -6,7 +6,15 @@ const SignUnScreen = () => {
 
     return (
         <div className="signupScreen">
-            
+            <form>
+                <h1>Sign In</h1>
+                <input type="email" placeholder="Email Addres" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Sign In</button>
+                <h4>
+                    <span className="signupScreen__gray">New to Netflix?</span>Sign up now.
+                </h4>
+            </form>
         </div>
     );
 }
